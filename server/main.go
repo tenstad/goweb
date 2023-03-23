8c973162ff15291e85c9182544030bacca8fe243
Amund Tenstad <github@amund.io>
2023-03-23 12:02:19 +0100

    Add request metric

--- server/main.go
+++ server/main.go
@@ -6,10 +6,17 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	httpRequestsProcessed := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "http_requests_processed",
+		Help: "The total number of processed http requests",
+	})
+
 	http.Handle("/metrics", promhttp.Handler())
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +28,7 @@ func main() {
 		fmt.Println(fmt.Sprintf("%s %s", end.Format("2006-01-02 15:04:05.000000"), end.Sub(start)))
 
 		fmt.Fprintf(w, "ok")
+		httpRequestsProcessed.Inc()
 	})
 
 	http.ListenAndServe(":8080", nil)
